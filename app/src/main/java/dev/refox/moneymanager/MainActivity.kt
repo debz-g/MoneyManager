c6cb2001e8ca865e3c1d2b7094b70c80b5ceef60
madhurmehta007 <12madhurmehta@gmail.com>
2023-01-01 20:03:05 +0530

    Made some UI changes in MainActivity.kt

--- app/src/main/java/dev/refox/moneymanager/MainActivity.kt
+++ app/src/main/java/dev/refox/moneymanager/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        window.setStatusBarColor(this.getResources().getColor(R.color.material_blue))
 
         binding.rvEmployeesList.layoutManager = LinearLayoutManager(this)
         binding.rvEmployeesList.setHasFixedSize(true)
